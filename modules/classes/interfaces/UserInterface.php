ad373d6c9f8f45b3913b0579295fc33b91ecb046
Obolduy <ivanox01@yandex.ru>
2021-03-24 12:21:24 +0300

    Code refactoring started

--- modules/classes/interfaces/UserInterface.php
+++ modules/classes/interfaces/UserInterface.php
@@ -2,6 +2,12 @@
 
 interface UserInterface
 {
+    /**
+	 * Registration user
+	 * @param void
+	 * @return registration.php if method GET and void if POST
+	 */
+
     public function registration();
 
     public function login();
