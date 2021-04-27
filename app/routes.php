31daae4a37b91b79e72c9d430524aa889d4ff4de
Obolduy <ivanox01@yandex.ru>
2021-04-27 11:31:14 +0300

    List of routes added

--- app/routes.php
+++ app/routes.php
@@ -3,7 +3,8 @@ use App\Router\Route;
 use App\Controllers\{
     MainPageController, ShowLotController, ShowUserController, RegistrationController, LoginController,
     DeleteLotController, ChangeLotController, ChangeUserController, AddLotController, AddCommentController,
-    ShowOtherUserController};
+    ShowOtherUserController, AdminLoginController, AdminPanelController, AdminChangeUserController, AdminChangeLotController,
+    AdminChangeCommentController, AdminDeleteUserController, AdminDeleteLotController, AdminDeleteCommentController};
 
 return [
     new Route('/', [MainPageController::class, 'showlots']),
@@ -19,5 +20,16 @@ return [
     new Route('/user', [ShowUserController::class, 'showuser'], 'authmiddleware'),
     new Route('/user/change', [ChangeUserController::class, 'changeInformation'], 'authmiddleware'),
     new Route('/users/{user_id}', [ShowUserController::class, 'showotheruser']),
+    new Route('/admin/login', [AdminLoginController::class, 'adminLogin']),
+    new Route('/admin', [AdminPanelController::class, 'showAdminPanel']),
+    new Route('/admin/users', [AdminChangeUserController::class, 'adminShowUsersTable']),
+    new Route('/admin/lots', [AdminChangeLotController::class, 'adminShowLotsTable']),
+    new Route('/admin/comments', [AdminChangeCommentController::class, 'adminShowCommentsTable']),
+    new Route('/admin/change/user/{user_id}', [AdminChangeUserController::class, 'adminChangeUser']),
+    new Route('/admin/change/lot/{lot_id}', [AdminChangeLotController::class, 'adminChangeLot']),
+    new Route('/admin/change/comment/{comment_id}', [AdminChangeCommentController::class, 'adminChangeComment']),
+    new Route('/admin/delete/user/{user_id}', [AdminDeleteUserController::class, 'adminDeleteUser']),
+    new Route('/admin/delete/lot/{lot_id}', [AdminDeleteLotController::class, 'adminDeleteLot']),
+    new Route('/admin/delete/comment/{comment_id}', [AdminDeleteCommentController::class, 'adminDeleteComment']),
     new Route('/index/df/{textinskobka}/{newtext}', [RegistrationController::class, 'testсont'], 'authmiddleware'),
 ];
