cdcca6e0bcd30d1f45449bf582b3adafe017dc44
Obolduy <ivanox01@yandex.ru>
2021-05-07 19:50:27 +0300

    changeuser added

---
+++ app/View/Views/changeuser.php
@@ -0,0 +1,16 @@
+<form method="POST">
+    <?php foreach($user as $elem): ?>
+    <div>Введите логин: <input type="text" name="login" value="<?= $elem['login'] ?>" required></div>
+    <div>Введите email: <input type="text" name="email" value="<?= $elem['email'] ?>" required></div>
+    <div>Введите имя: <input type="text" name="name" value="<?= $elem['name'] ?>" required></div>
+    <div>Введите фамилию: <input type="text" name="surname" value="<?= $elem['surname'] ?>" required></div>
+    <div>Введите пароль: <input type="password" name="password" required></div>
+    <div>Подтвердите пароль: <input type="password" name="confirmPassword" required></div>
+    <div><select name="category_id">
+        <?php foreach ($cities as $city): ?>
+        <option value="<?= $city['id'] ?>" <?php if ($elem['city_id'] == $city['id']) {echo 'selected';} ?>><?= $city['city']; ?></option>
+        <?php endforeach; ?>
+    </select></div>
+    <?php endforeach; ?>
+    <div><input type="submit" name="submit"></div>
+</form>
--- app/controllers/ChangeUserController.php
+++ app/controllers/ChangeUserController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controllers;
 use App\Models\User;
+use App\Models\Model;
 use App\View\View;
 
 class ChangeUserController
@@ -11,14 +12,15 @@ class ChangeUserController
 	 * @return void
 	 */
 
-    public static function changeInformation(int $user_id): void
+    public static function changeInformation(): void
     {
         $user = new User();
 
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            $user = $user->getFullUserInfo($user_id);
+            $user = $user->getFullUserInfo($_SESSION['user']['id']);
+            $cities = (new Model)->getAll('cities');
 
-            new View('changelot', ['lot' => $lot, 'categories' => $categories, 'title' => 'Изменить данные']);
+            new View('changeuser', ['user' => $user, 'cities' => $cities, 'title' => 'Изменить данные']);
         } else {
             $check = User::changeCheck(strip_tags($_POST['login']), strip_tags($_POST['password']),
                 strip_tags($_POST['confirmPassword']), $_SESSION['user']['login'], strip_tags($_POST['email']));
@@ -26,13 +28,13 @@ class ChangeUserController
             if ($check == true) {
                 $cryptpassword = password_hash(strip_tags($_POST['password']), PASSWORD_DEFAULT);
                 
-                $data = [strip_tags($_POST['login']), $cryptpassword, strip_tags($_POST['email']), $user_id];
+                $data = [strip_tags($_POST['login']), $cryptpassword, strip_tags($_POST['email']), $_SESSION['user']['id']];
 
                 $user->update("UPDATE users SET login = ?, password = ?, email = ?, updated_at = now() WHERE id = ?", $data);
-                $user->update("UPDATE names SET name = ? WHERE user_id = ?", [strip_tags($_POST['name']), $user_id]);
-                $user->update("UPDATE surnames SET surname = ? WHERE user_id = ?", [strip_tags($_POST['surname']), $user_id]);
+                $user->update("UPDATE names SET name = ? WHERE user_id = ?", [strip_tags($_POST['name']), $_SESSION['user']['id']]);
+                $user->update("UPDATE surnames SET surname = ? WHERE user_id = ?", [strip_tags($_POST['surname']), $_SESSION['user']['id']]);
 
-                $user_info = $user->getOne('users', $user_id);
+                $user_info = $user->getOne('users', $_SESSION['user']['id']);
 
                 foreach($user_info as $elem) {
                     $user->setData($elem['id']);
