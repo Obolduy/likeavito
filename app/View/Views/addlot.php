450e48fc9524e4b42b8adeb8e0587bd2e7deff26
Obolduy <ivanox01@yandex.ru>
2021-07-08 18:43:12 +0300

    Remake adding lots and user pics system

--- app/View/Views/addlot.php
+++ app/View/Views/addlot.php
@@ -1,4 +1,12 @@
-<form method="POST">
+<?php
+if ($_SESSION['addlot_err_msg']) {
+    foreach ($_SESSION['addlot_err_msg'] as $elem) {
+        echo $elem . '<br>';
+    }
+    unset($_SESSION['addlot_err_msg']);
+}
+?>
+<form method="POST" enctype="multipart/form-data">
     <div>Введите название: <input type="text" name="title" required></div>
     <div>Введите цену: <input type="text" name="price" required></div>
     <div>Введите описание: <textarea name="description"></textarea></div>
@@ -7,5 +15,6 @@
         <option value="<?= $category['id'] ?>"><?= $category['category']; ?></option>
         <?php endforeach; ?>
     </select></div>
+    <div class="photos">Добавьте изображение: <input type="file" accept="image/*" name="photos[]" multiple></div>
     <div><input type="submit" name="submit"></div>
 </form>
