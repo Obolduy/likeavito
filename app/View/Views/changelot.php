9aa0f192d7f6d0773c767e593467993bcbd697db
Obolduy <ivanox01@yandex.ru>
2021-07-28 19:47:48 +0300

    Cosmetic changes

--- app/View/Views/changelot.php
+++ app/View/Views/changelot.php
@@ -1,5 +1,12 @@
+<?php
+if ($_SESSION['lot_err_msg']) {
+    echo $_SESSION['lot_err_msg'];
+
+    unset($_SESSION['lot_err_msg']);
+}
+?>
 <form enctype="multipart/form-data" method="POST">
-    <?php foreach($lot as $elem): ?>
+    <?php foreach ($lot as $elem): ?>
     <div>Введите название: <input type="text" name="title" value="<?= $elem['title'] ?>" required></div>
     <div>Введите цену: <input type="text" name="price" value="<?= $elem['price'] ?>" required></div>
     <div>Введите описание: <textarea name="description"><?= $elem['description'] ?></textarea></div>
--- app/View/Views/changeuser.php
+++ app/View/Views/changeuser.php
@@ -1,15 +1,22 @@
+<?php
+if ($_SESSION['user_err_msg']) {
+    echo $_SESSION['user_err_msg'];
+
+    unset($_SESSION['user_err_msg']);
+}
+?>
 <form enctype="multipart/form-data" method="POST">
-    <?php foreach($user as $elem): ?>
+    <?php foreach ($user as $elem): ?>
     <div>Введите логин: <input type="text" name="login" value="<?= $elem['login'] ?>" required></div>
     <div>Введите email: <input type="text" name="email" value="<?= $elem['email'] ?>" required></div>
     <div>Обновите Ваш аватар (Необязательно): <input type="file" accept="image/*" name="photo"></div>
     <div>Введите имя: <input type="text" name="name" value="<?= $elem['name'] ?>" required></div>
     <div>Введите фамилию: <input type="text" name="surname" value="<?= $elem['surname'] ?>" required></div>
-    <div>Введите пароль: <input type="password" name="password" required></div>
-    <div>Подтвердите пароль: <input type="password" name="confirmPassword" required></div>
-    <div><select name="category_id">
+    <div>Введите новый пароль (по желанию): <input type="password" name="password"></div>
+    <div>Подтвердите пароль (по желанию): <input type="password" name="confirmPassword"></div>
+    <div><select name="city_id">
         <?php foreach ($cities as $city): ?>
-        <option value="<?= $city['id'] ?>" <?php if ($elem['city_id'] == $city['id']) {echo 'selected';} ?>><?= $city['city']; ?></option>
+        <option value="<?= $city['id'] ?>" <?php if ($elem['city_id'] == $city['id']) echo 'selected'; ?>><?= $city['city']; ?></option>
         <?php endforeach; ?>
     </select></div>
     <?php endforeach; ?>
