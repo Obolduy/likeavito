2832d88aeb055625eeabeae3eb78b8d8d2def7ca
Obolduy <ivanox01@yandex.ru>
2021-09-11 19:35:44 +0300

    ShowUserController

--- app/controllers/ShowUserController.php
+++ app/controllers/ShowUserController.php
@@ -1,38 +1,37 @@
 <?php
 namespace App\Controllers;
 
-use App\Models\User;
-use App\Models\Lots;
-use App\Models\Comments;
+use App\Models\UserAuth;
+use App\Models\LotGet;
+use App\Models\CommentGet;
+use App\Models\UserGet;
 use App\View\View;
 
 class ShowUserController
 {   
     public static function showUser(): void
     {
-        $user = (new User)->getFullUserInfo($_SESSION['user']['id']);
-
-        new View('showuser', ['user' => $user, 'title' => $user[0]['login']]);
+        new View('showuser', ['user' => (new UserAuth)->data, 'title' => (new UserAuth)->data['login']]);
     }
 
     public static function showUsersLots(): void
     {
-        $lots = (new Lots)->getOne('lots', $_SESSION['user']['id'], 'owner_id');
+        $lots = (new LotGet)->getUserLots($_SESSION['user_id']);
 
         new View('showuserslots', ['lots' => $lots, 'title' => "Ваши товары"]);
     }
 
     public static function showUsersComments(): void
     {
-        $comments = (new Comments)->getOne('comments', $_SESSION['user']['id'], 'user_id');
+        $comments = (new CommentGet)->getCommentsByUserId($_SESSION['user_id']);
 
         new View('showuserscomments', ['comments' => $comments, 'title' => "Ваши комментарии"]);
     }
 
     public static function showOtherUser(int $user_id): void
     {
-        $user = (new User)->getFullUserInfo($user_id);
+        $user = (new UserGet)->getOtherUser($user_id);
 
-        new View('showotheruser', ['user' => $user, 'title' => "Просмотр пользователя {$user[0]['login']}"]);
+        new View('showotheruser', ['user' => $user, 'title' => "Просмотр пользователя {$user['login']}"]);
     }
 }
