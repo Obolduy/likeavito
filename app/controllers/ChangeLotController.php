9d76cea5c9d977e2322bb7ec5081bf229efae157
Obolduy <ivanox01@yandex.ru>
2021-09-12 21:34:09 +0300

    Fixes in Lot manipulate and change lot controller

--- app/controllers/ChangeLotController.php
+++ app/controllers/ChangeLotController.php
@@ -2,8 +2,9 @@
 namespace App\Controllers;
 
 use App\Models\LotGet;
-use App\Models\MySQLDB;
-use App\Models\LotChange;
+use App\Models\Categories;
+use App\Models\LotManipulate;
+use App\Models\LotValidate;
 use App\View\View;
 
 class ChangeLotController
@@ -12,21 +13,23 @@ class ChangeLotController
     {
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             $lot = (new LotGet)->getFullLotInfo($lot_id);
-            $categories = (new MySQLDB)->dbQuery('SELECT * FROM lots_category')->fetchAll();
+            $categories = (new Categories)->getAllCategories();
 
             new View('changelot', ['lot' => $lot, 'categories' => $categories, 'title' => 'Изменить товар']);
         } else {
-            if (!is_numeric(strip_tags($_POST['price']))) {
+            $check = (new LotValidate)->checkLotData($_POST['title'], $_POST['price']);
+
+            if ($check !== true) {
                 $_SESSION['lot_err_msg'] = 'Цена должна быть записана числом';
                 
                 header("Location: /managelot/$lot_id/change");
-            }
-
-            (new LotChange($lot_id))->changeLot(strip_tags($_POST['title']), strip_tags($_POST['price']),
+            } else {
+                (new LotManipulate)->changeLot($lot_id, strip_tags($_POST['title']), strip_tags($_POST['price']),
                     strip_tags($_POST['description'], '<p></p><br/><br><i><b><s><u><strong>'),
                         $_POST['category_id'], $_FILES['photos']);
 
-            header("Location: /category/{$_POST['category_id']}/$lot_id");
+                header("Location: /category/{$_POST['category_id']}/$lot_id");
+            }
         }
     }
 }
--- app/models/LotManipulate.php
+++ app/models/LotManipulate.php
@@ -22,16 +22,16 @@ class LotManipulate
                 [$ownerId, $categoryId, $title, $price, $description]);
     }
 
-    public function changeLot(int $lotId, string $title, int $price, string $description, int $categoryId, array $pictures = null)
+    public function changeLot(int $lotId, string $title, int $price, string $description, int $categoryId, int $display, array $pictures = null)
     {
         if ($pictures) {
             $this->picture->uploadPicture("lots/$lotId", $pictures);
         }
 
-        $this->db->dbQuery("UPDATE lots SET title = ?, price = ?, description = ?, category_id = ?, update_time = now() WHERE id = ?",
+        $this->db->dbQuery("UPDATE lots SET title = ?, price = ?, description = ?, category_id = ?, display = ? update_time = now() WHERE id = ?",
             [strip_tags($_POST['title']), strip_tags($_POST['price']),
                 strip_tags($title, $price, $description, '<p></p><br/><br><i><b><s><u><strong>'),
-                    $categoryId, $lotId]);
+                    $categoryId, $display, $lotId]);
     }
 
     public function deleteLot(int $lotId): void
