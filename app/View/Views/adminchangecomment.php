52bd45a70ab904756fd232489ce81430062fb292
Obolduy <ivanox01@yandex.ru>
2021-09-18 16:12:32 +0300

    adminchangecomment view

--- app/View/Views/adminchangecomment.php
+++ app/View/Views/adminchangecomment.php
@@ -1,7 +1,5 @@
 <form method="POST">
-    <?php foreach($comment as $elem): ?>
-    <div>Введите описание: <textarea name="description"><?= $elem['description'] ?></textarea></div>
-    <?php endforeach; ?>
+    <div>Введите описание: <textarea name="description"><?= $comment['description'] ?></textarea></div>
     <div><input type="submit" name="submit"></div>
 </form>
-<div class="admin_comments"><a href="/admin/delete/comment/<?= $elem['id'] ?>">Удалить комменатрий</a></div>
+<div class="admin_comments"><a href="/admin/delete/comment/<?= $comment['id'] ?>">Удалить комменатрий</a></div>
