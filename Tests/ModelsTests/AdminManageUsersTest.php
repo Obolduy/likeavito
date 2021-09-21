261a848009ec405d8228014cac9bc791a4a7312b
Obolduy <ivanox01@yandex.ru>
2021-09-21 18:53:33 +0300

    Adminmanageuserstest

---
+++ Tests/ModelsTests/AdminManageUsersTest.php
@@ -0,0 +1,83 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Models\AdminManageUsers;
+use App\Models\UserGet;
+
+class AdminManageUsersTest extends TestCase
+{
+    private $adminManageUsers;
+    private $userGet;
+
+    protected function setUp(): void 
+    {
+        $this->adminManageUsers = new AdminManageUsers();
+        $this->userGet = new UserGet();
+    }
+
+    public function manageUsersProvider()
+    {
+        return [
+            [66, 1],
+            [67, 1],
+            [66, 0],
+            [67, 0]
+        ];
+    }
+
+    public function adminUndoProvider()
+    {
+        return [
+            [66, 55],
+            [67, 55],
+            [66, 66],
+            [67, 66]
+        ];
+    }
+
+    /**
+     * @dataProvider manageUsersProvider
+     */
+
+    public function testUserBanManage(int $userId, int $banStatus)
+    {
+        $this->adminManageUsers->userBanManage($userId, $banStatus);
+
+        $test = $this->userGet->getUserInfo($userId);
+        $this->assertEquals($banStatus, $test['ban_status']);
+    }
+
+    /**
+     * @dataProvider manageUsersProvider
+     */
+
+    public function testMakeUserAnAdmin(int $userId, int $banStatus)
+    {
+        $this->adminManageUsers->makeUserAnAdmin($userId);
+
+        $test = $this->userGet->getUserInfo($userId);
+        $this->assertEquals(2, $test['status_id']);
+    }
+
+    /**
+     * @dataProvider adminUndoProvider
+     */
+
+    public function testUndoUserAsAnAdmin(int $userId, int $sessionId)
+    {
+        $_SESSION['user_id'] = $sessionId;
+        
+        $test = $this->adminManageUsers->undoUserAsAnAdmin($userId);
+
+        if ($sessionId == $userId) {
+            $this->assertFalse($test);
+        } else {
+            $this->assertTrue($test);
+        }
+    }
+
+    protected function tearDown(): void
+    {
+        $this->adminManageUsers = NULL;
+        $this->userGet = NULL;
+    }
+}
