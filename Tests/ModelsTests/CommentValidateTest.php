d51117e80806df6694866090e53a65f9b7816437
Obolduy <ivanox01@yandex.ru>
2021-09-22 22:37:13 +0300

    CommentValidatetest + fix

---
+++ Tests/ModelsTests/CommentValidateTest.php
@@ -0,0 +1,42 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Models\CommentValidate;
+
+use function PHPUnit\Framework\assertEmpty;
+use function PHPUnit\Framework\assertEquals;
+
+class CommentValidateTest extends TestCase
+{
+    private $commentValidate;
+
+    protected function setUp(): void 
+    {
+        $this->commentValidate = new CommentValidate();
+    }
+
+    public function checkCommentProvider()
+    {
+        return [
+            ['Test', true],
+            ['Test1234', true],
+            [132424, ['Пожалуйста, напишите Ваш комментарий, используя слова']],
+            ['Пожалуйста, используйте текст', true],
+            ['\\@#$%^&*&^%$#$%^&*(', ['Пожалуйста, напишите Ваш комментарий, используя слова']]
+        ];
+    }
+
+    /**
+     * @dataProvider checkCommentProvider
+     */
+
+    public function testCheckComment($text, $expected)
+    {
+        $test = $this->commentValidate->checkComment($text);
+        $this->assertEquals($expected, $test);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->commentValidate = NULL;
+    }
+}
--- app/models/CommentValidate.php
+++ app/models/CommentValidate.php
@@ -7,8 +7,8 @@ class CommentValidate extends Model
 
     public function checkComment($text)
     {
-        if (is_numeric($text)) {
-            $this->errorArray[] = 'Пожалуйста, используйте текст';
+        if (is_numeric($text) || !preg_match('#[A-Za-zА-Яа-я]+#u', $text)) {
+            $this->errorArray[] = 'Пожалуйста, напишите Ваш комментарий, используя слова';
         }
 
         if (!empty($this->errorArray)) {
