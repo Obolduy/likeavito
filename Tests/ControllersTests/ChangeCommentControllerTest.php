eedfd211886bebf5c71c84203852e9d93bfdafc2
Obolduy <ivanox01@yandex.ru>
2021-10-02 23:10:01 +0300

    Changecommentcontroller test

--- Tests/ControllersTests/ChangeCommentControllerTest.php
+++ Tests/ControllersTests/ChangeCommentControllerTest.php
@@ -1,26 +1,26 @@
 <?php
 use PHPUnit\Framework\TestCase;
-use App\Models\Comments;
+use App\Models\Database;
 use App\Controllers\ChangeCommentController;
 
 class ChangeCommentControllerTest extends TestCase
 {
     private $changeCommentController;
-    private $comment;
+    private $database;
 
     protected function setUp(): void 
     {
         $this->changeCommentController = new ChangeCommentController();
-        $this->comment = new Comments();
+        $this->database = new Database();
     }
 
     public function changeCommentProvider()
     {
         return [
-            [2, 'NewComment001'],
-            [3, 'NewComment002'],
-            [4, 'NewComment003'],
-            [5, 'NewComment004']
+            [2, 'NewComment001', 50],
+            [3, 'NewComment002', 23],
+            [4, 'NewComment003', 3],
+            [5, 'NewComment004', 1]
         ];
     }
 
@@ -28,22 +28,20 @@ class ChangeCommentControllerTest extends TestCase
      * @dataProvider changeCommentProvider
      */
 
-    public function testChangeComment($comment_id, $description) 
+    public function testChangeComment($commentId, $description, $userId) 
     {
         $_POST['description'] = $description;
+        $_SESSION['user']['id'] = $userId;
 
-        $this->changeCommentController->changeComment($comment_id);
+        $this->changeCommentController->changeComment($commentId);
 
-        $data = $this->comment->getOne('comments', $description, 'description');
-
-        foreach ($data as $elem) {
-            $this->assertEquals($description, $elem['description']);
-        }
+        $test = $this->database->dbQuery("SELECT * FROM comments WHERE id = $commentId")->fetch();
+        $this->assertEquals($description, $test['description']);
     }
 
     protected function tearDown(): void
     {
         $this->changeCommentController = NULL;
-        $this->comment = NULL;
+        $this->database = NULL;
     }
 }
