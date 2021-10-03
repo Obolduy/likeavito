25ecfaa7913e7e29d52e9dcc0cefd92df9777e7b
Obolduy <ivanox01@yandex.ru>
2021-10-03 16:00:03 +0300

    Change comment controller fix

--- app/controllers/ChangeCommentController.php
+++ app/controllers/ChangeCommentController.php
@@ -20,11 +20,12 @@ class ChangeCommentController
             new View('changecomment', ['title' => 'Изменить комментарий', 'comment' => $comment]);
         } else {
             $text = strip_tags($_POST['description'], '<p></p><br/><br><i><b><s><u><strong>');
-
+            $display = $_POST['display'] ?? 0;
+            
             $validate = (new CommentValidate)->checkComment($text);
 
             if (is_bool($validate)) {
-                (new CommentManipulate)->changeComment($comment_id, 1, $text);
+                (new CommentManipulate)->changeComment($comment_id, $display, $text);
 
                 header("Location:" . $_SESSION['http_referer']);
                 
