61fae201d0cb2fa3a57a02976007a070e2250478
Obolduy <ivanox01@yandex.ru>
2021-10-04 17:20:29 +0300

    changelot view

--- app/View/Views/changelot.php
+++ app/View/Views/changelot.php
@@ -6,16 +6,14 @@ if ($_SESSION['lot_err_msg']) {
 }
 ?>
 <form enctype="multipart/form-data" method="POST">
-    <?php foreach ($lot as $elem): ?>
-    <div>Введите название: <input type="text" name="title" value="<?= $elem['title'] ?>" required></div>
-    <div>Введите цену: <input type="text" name="price" value="<?= $elem['price'] ?>" required></div>
-    <div>Введите описание: <textarea name="description"><?= $elem['description'] ?></textarea></div>
+    <div>Введите название: <input type="text" name="title" value="<?= $lot['title'] ?>" required></div>
+    <div>Введите цену: <input type="text" name="price" value="<?= $lot['price'] ?>" required></div>
+    <div>Введите описание: <textarea name="description"><?= $lot['description'] ?></textarea></div>
     <div><select name="category_id">
         <?php foreach ($categories as $category): ?>
-        <option value="<?= $category['id'] ?>" <?php if ($elem['category_id'] == $category['id']) {echo 'selected';} ?>><?= $category['category']; ?></option>
+        <option value="<?= $category['id'] ?>" <?php if ($lot['category_id'] == $category['id']) {echo 'selected';} ?>><?= $category['category']; ?></option>
         <?php endforeach; ?>
     </select></div>
-    <?php endforeach; ?>
     <div>Загрузите картинки (Необязательно): <input type="file" accept="image/*" name="photos[]" multiple></div>
     <div><input type="checkbox" name="display" value="1" checked> Отображать лот</div>
     <div><input type="submit" name="submit"></div>
