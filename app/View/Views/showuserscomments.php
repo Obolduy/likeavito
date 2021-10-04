7ae77cf426f9a2435e85d139374873ee2563876e
Obolduy <ivanox01@yandex.ru>
2021-10-04 22:07:06 +0300

    JS now asking for delete comments\lots

--- app/View/Views/showuserscomments.php
+++ app/View/Views/showuserscomments.php
@@ -10,8 +10,17 @@
             <td><?= $comment['description'] ?></td>
             <td><?= $comment['add_time'] ?></td>
             <td><a href="/changecomment/<?= $comment['id'] ?>">Редактировать</a><br>
-            <a href="/deletecomment/<?= $comment['id'] ?>">Удалить</a></td>
+            <a onclick="confirmDeleteComment()" href="/deletecomment/<?= $comment['id'] ?>">Удалить</a></td>
         </tr>
         <?php endforeach; ?>
     </table>
 </div>
+<script type="text/javascript">
+    function confirmDeleteComment() {
+        if (confirm("Вы уверены, что хотите удалить комментарий?")) {
+            return true;
+        } else {
+            event.preventDefault();
+        }
+    }
+</script>
--- app/View/Views/showuserslots.php
+++ app/View/Views/showuserslots.php
@@ -10,8 +10,17 @@
             <td><a href="/category/<?= $lot['category_id'] ?>/<?= $lot['id'] ?>"><?= $lot['title'] ?></a></td>
             <td><?= $lot['add_time'] ?></td>
             <td><a href="/managelot/<?= $lot['id'] ?>/change">Редактировать</a><br>
-            <a href="/managelot/<?= $lot['id'] ?>/delete">Удалить</a></td>
+            <a onclick="confirmDeleteLot()" href="/managelot/<?= $lot['id'] ?>/delete">Удалить</a></td>
         </tr>
         <?php endforeach; ?>
     </table>
 </div>
+<script type="text/javascript">
+    function confirmDeleteLot() {
+        if (confirm("Вы уверены, что хотите удалить товар <?php echo $lot['title']; ?>?")) {
+            return true;
+        } else {
+            event.preventDefault();
+        }
+    }
+</script>
