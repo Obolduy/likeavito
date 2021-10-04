1f6bf1730f19333d28c4a67e51555061c0fad153
Obolduy <ivanox01@yandex.ru>
2021-10-04 17:21:13 +0300

    Change and delete lot controllers

--- app/controllers/ChangeLotController.php
+++ app/controllers/ChangeLotController.php
@@ -6,12 +6,16 @@ use App\Models\Categories;
 use App\Models\LotManipulate;
 use App\Models\LotValidate;
 use App\View\View;
+use Predis\Autoloader;
+use Predis\Client;
 
 class ChangeLotController
 {   
     public static function changeLot(int $lot_id): void
     {
-        $lot = (new LotGet)->getFullLotInfo($lot_id);
+        $lotGet = new LotGet();
+
+        $lot = $lotGet->getFullLotInfo($lot_id);
 
         if ($lot['LotInfo']['owner_id'] != $_SESSION['user']['id']) {
             header('Location: /'); die();
@@ -20,7 +24,7 @@ class ChangeLotController
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             $categories = (new Categories)->getAllCategories();
 
-            new View('changelot', ['lot' => $lot, 'categories' => $categories, 'title' => 'Изменить товар']);
+            new View('changelot', ['lot' => $lot['LotInfo'], 'categories' => $categories, 'title' => 'Изменить товар']);
         } else {
             $check = (new LotValidate)->checkLotData(strip_tags($_POST['title']), strip_tags($_POST['price']));
 
@@ -32,6 +36,17 @@ class ChangeLotController
                 (new LotManipulate)->changeLot($lot_id, strip_tags($_POST['title']), strip_tags($_POST['price']),
                     strip_tags($_POST['description'], '<p></p><br/><br><i><b><s><u><strong>'),
                         $_POST['category_id'], $_POST['display'], $_FILES['photos']);
+
+                $lots = $lotGet->getLotsForCache();
+            
+                Autoloader::register();
+                $cache = new Client();
+
+                for ($i = 1; $i <= 5; $i++) {
+                    $cache->hmset("new_lots", [
+                        "link_$i" => "<a href=\"/category/{$lots[$i - 1]['category_id']}/{$lots[$i - 1]['id']}\">{$lots[$i - 1]['title']}</a>"
+                    ]);
+                }
 
                 header("Location: /category/{$_POST['category_id']}/$lot_id");
             }
--- app/controllers/DeleteLotController.php
+++ app/controllers/DeleteLotController.php
@@ -3,19 +3,34 @@ namespace App\Controllers;
 
 use App\Models\LotManipulate;
 use App\Models\LotGet;
+use Predis\Autoloader;
+use Predis\Client;
 
 class DeleteLotController
 {   
     public static function deleteLot(int $lot_id): void
     {
-        $checkUser = (new LotGet)->getFullLotInfo($lot_id);
+        $lotGet = new LotGet();
 
-        if ($checkUser['owner_id'] == $_SESSION['user']['id']) {
+        $checkUser = $lotGet->getFullLotInfo($lot_id);
+
+        if ($checkUser['LotInfo']['owner_id'] == $_SESSION['user']['id']) {
             (new LotManipulate)->deleteLot($lot_id);
+
+            $lots = $lotGet->getLotsForCache();
+            
+            Autoloader::register();
+            $cache = new Client();
+
+            for ($i = 1; $i <= 5; $i++) {
+                $cache->hmset("new_lots", [
+                    "link_$i" => "<a href=\"/category/{$lots[$i - 1]['category_id']}/{$lots[$i - 1]['id']}\">{$lots[$i - 1]['title']}</a>"
+                ]);
+            }
 
             header('Location: /'); die();
         }
 
-        header('Location: /user');
+        header('Location: /user/showlots');
     }
 }
