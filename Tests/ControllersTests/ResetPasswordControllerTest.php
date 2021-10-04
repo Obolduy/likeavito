6fd382440a7feb302d4ee1eef370f601abe88e0e
Obolduy <ivanox01@yandex.ru>
2021-10-04 12:58:21 +0300

    reset password controller test

--- Tests/ControllersTests/ResetPasswordControllerTest.php
+++ Tests/ControllersTests/ResetPasswordControllerTest.php
@@ -1,7 +1,7 @@
 <?php
 use PHPUnit\Framework\TestCase;
-use App\Models\User;
 use App\Controllers\ResetPasswordController;
+use App\Models\Database;
 
 class ResetPasswordControllerTest extends TestCase
 {
@@ -11,7 +11,7 @@ class ResetPasswordControllerTest extends TestCase
     protected function setUp(): void 
     {
         $this->resetPasswordController = new ResetPasswordController();
-        $this->user = new User();
+        $this->user = new Database();
     }
 
     public function resetRequestProvider()
@@ -25,7 +25,6 @@ class ResetPasswordControllerTest extends TestCase
     }
 
     /**
-     * Tests clear, but this one don`t return $stack and return error
      * @dataProvider resetRequestProvider
      */
 
@@ -33,41 +32,30 @@ class ResetPasswordControllerTest extends TestCase
     {
         $_POST['email'] = $email;
 
-        $test = $this->resetPasswordController->resetRequest($email);
+        $this->resetPasswordController->resetRequest($email);
 
-        $dir = scandir('C:\\openserver\\userdata\\temp\\email');
-        $file = $dir[count($dir) - 1];
-        $file = file("C:\\openserver\\userdata\\temp\\email\\$file")[13];
-        preg_match('#http://likeavito/user/resetpassword/(.+)#', $file, $link);
+        $file = file('/Users/vladislav/projects/maillog.txt');
 
-        $check_token = $this->user->getOne('password_reset', trim($link[1]), 'token');
-
-        $this->assertNotNull($check_token);
-
-        foreach ($check_token as $elem) {
-            $stack = ['token' => $elem['token'], 'email' => $email];
-        }
-
-        $this->testPasswordResetForm($stack);
-        return $stack;
+        $lastString = $file[(count($file) - 1)];
+        $test = str_contains($lastString, $email);
+        $this->assertTrue($test);
     }
 
     /**
-     * @depends testResetRequest
+     * @dataProvider resetRequestProvider
      */
 
-    public function testPasswordResetForm($stack) 
+    public function testPasswordResetForm($email) 
     {
         $_POST['password'] = 222222;
         $_POST['confirmPassword'] = 222222;
+        $token = 'fjnfrenfrefnerfnw3243';
 
-        $this->resetPasswordController->passwordResetForm($stack['token']);
+        $this->resetPasswordController->passwordResetForm($token);
 
-        $test = $this->user->getOne('users', $stack['email'], 'email');
+        $test = $this->user->dbQuery("SELECT password FROM users WHERE email = ?", [$email])->fetchColumn();
 
-        foreach ($test as $elem) {
-            $this->assertTrue(password_verify($_POST['password'], $elem['password']));
-        }
+        $this->assertTrue(password_verify($_POST['password'], $test));
     }
 
     protected function tearDown(): void
