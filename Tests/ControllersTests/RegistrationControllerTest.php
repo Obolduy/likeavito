631f94a90a342c0634954d7b9c85370bfadd8961
Obolduy <ivanox01@yandex.ru>
2021-10-04 12:23:04 +0300

    registration controller test

--- Tests/ControllersTests/RegistrationControllerTest.php
+++ Tests/ControllersTests/RegistrationControllerTest.php
@@ -1,20 +1,14 @@
 <?php
 use PHPUnit\Framework\TestCase;
-use App\Models\Model;
-use App\Models\User;
 use App\Controllers\RegistrationController;
 
 class RegistrationControllerTest extends TestCase
 {
     private $registrationController;
-    private $user;
-    private $model;
 
     protected function setUp(): void 
     {
         $this->registrationController = new RegistrationController();
-        $this->user = new User();
-        $this->model = new Model();
     }
 
     /**
@@ -30,58 +24,15 @@ class RegistrationControllerTest extends TestCase
         $_POST['name'] = 'Name';
         $_POST['surname'] = 'Surname';
         $_POST['city_id'] = '2';
+        $_SERVER['REQUEST_METHOD'] = 'POST';
 
-        $registration = RegistrationController::registration();
+        $this->registrationController->registration();
 
-        $data = $this->model->getOne('users', $_POST['email'], 'email');
-
-        foreach($data as $elem) {
-            $result = $elem['id'];
-        }
-
-        $this->assertEquals(20, $result);
-    }
-
-    public function testLogIn() 
-    {
-        $_POST['login'] = 'logsin';
-        $_POST['password'] = '123456789';
-
-        $reg = Authorization::logIn();
-
-        $data = $this->base->getOne('users', 'logsin', 'login');
-
-        foreach ($data as $elem) {
-            $user = new User($elem['id']);
-        }
-
-        $this->assertEquals(4, $user->data['id']);
-    }
-
-    public function testRegistrationCheck() 
-    {
-        $login = 'logi58n';
-        $password = '123456';
-        $confirmPassword = '123456';
-
-        $result = Authorization::registrationCheck($login, $password, $confirmPassword);
-
-        $this->assertEquals(true, $result);
-    }
-    
-    public function testAuthCheck() 
-    {
-        $login = 'logsin';
-        $password = '123456';
-
-        $result = Authorization::authCheck($login, $password);
-
-        $this->assertEquals(true, $result);
+        $this->assertNotNull($_SESSION['user']);
     }
 
     protected function tearDown(): void
     {
         $this->registrationController = NULL;
-        $this->user = NULL;
     }
 }
