42346e5364dd65378d2104d6e0ee80b4f91a790c
Obolduy <ivanox01@yandex.ru>
2021-10-05 16:15:10 +0300

    managedisplaylot method in adminmanagelots

--- app/models/AdminManageLots.php
+++ app/models/AdminManageLots.php
@@ -3,13 +3,8 @@ namespace App\Models;
 
 class AdminManageLots extends Model
 {
-    public function hideLot(int $lotId): void
+    public function manageDisplayLot(int $display, int $lotId): void
     {
-        $this->db->dbQuery("UPDATE lots SET display = 0 WHERE id = ?", [$lotId]);
-    }
-
-    public function unhideLot(int $lotId): void
-    {
-        $this->db->dbQuery("UPDATE lots SET display = 1 WHERE id = ?", [$lotId]);
+        $this->db->dbQuery("UPDATE lots SET display = ? WHERE id = ?", [$display, $lotId]);
     }
 }
