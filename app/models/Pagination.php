76a676f9fd2bf4f6148354842672141be6b7191e
Obolduy <ivanox01@yandex.ru>
2021-10-05 16:12:46 +0300

    pagination rewrite

--- app/models/Pagination.php
+++ app/models/Pagination.php
@@ -4,53 +4,32 @@ namespace App\Models;
 class Pagination extends Model
 {
     public $table, $pageCount;
-    private $tableName, $columnName, $property, $border1, $border2, $count;
+    private $border1, $border2, $count, $query;
 
-    public function __construct(string $tableName, int $border1, int $border2 = 5)
+    public function __construct(int $border1, int $border2 = 5)
     {
         parent::__construct();
-        $this->tableName = $tableName;
         $this->border1 = $border1;
         $this->border2 = $border2;
     }
 
-    public function pagination(?string $columnName = null, ?string $property = null)
+    public function pagination(string $query)
     {
-        if ($columnName !== null && $property !== null) {
-            $this->columnName = $columnName;
-            $this->property = $property;
-            $this->table = $this->getTable();
-        } else {
-            $this->table = $this->getWholeTable();
-        }
+        $this->query = $query;
+
         $this->pageCount = $this->getPageCount();
-    }
 
-    private function getWholeTable(): array
-    {   
-        return $this->db->dbQuery("SELECT * FROM {$this->tableName} LIMIT {$this->border1}, {$this->border2}")
-                ->fetchAll();
-    }
-
-    private function getTable(): array
-    {   
-        return $this->db->dbQuery("SELECT * FROM {$this->tableName} WHERE {$this->columnName} = ? LIMIT {$this->border1}, {$this->border2}",
-                [$this->property])->fetchAll();
+        $this->table = $this->db->dbQuery("$query LIMIT {$this->border1}, {$this->border2}")->fetchAll();
     }
 
     private function getPageCount()
     {
-        if ($this->columnName != null) {
-            $this->count = $this->db->dbQuery("SELECT COUNT(*) FROM {$this->tableName} WHERE {$this->columnName} = ?",
-                [$this->property])->fetchColumn();
-        } else {
-            $this->count = $this->db->dbQuery("SELECT COUNT(*) FROM {$this->tableName}")->fetchColumn();
-        }
+        $this->count = count($this->db->dbQuery($this->query)->fetchAll());
 
         while ($this->count % 5 != 0) {
             $this->count++;
         }
 
-        return $this->count / 5;
+        return $this->count /= 5;
     }
 }
