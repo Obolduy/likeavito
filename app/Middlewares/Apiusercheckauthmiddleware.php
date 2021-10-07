398b1b9e5ab85cea1472307ed6d28fb6efe2a476
Obolduy <ivanox01@yandex.ru>
2021-10-07 21:59:41 +0300

    Apiusercheckauthmiddleware

--- app/Middlewares/Apiusercheckauthmiddleware.php
+++ app/Middlewares/Apiusercheckauthmiddleware.php
@@ -2,19 +2,20 @@
 namespace App\Middlewares;
 
 use App\Middlewares\IMiddleware;
-use App\Models\Model;
+use App\Models\Database;
 
 class Apiusercheckauthmiddleware implements IMiddleware
 {    
     public function middleware($uri)
     {
-        preg_match('#/[0-9]+#', $_SERVER['REQUEST_URI'], $lot_id);
-        $lot_id = preg_replace('#/#', '', $lot_id);
+        preg_match('#/[0-9]+#', $_SERVER['REQUEST_URI'], $lotId);
+        $lotId = preg_replace('#/#', '', $lotId);
 
-        $model = new Model();
-        $lot_owner = $model->getOne('lots', $lot_id[0]);
+        $database = new Database();
+
+        $lotOwner = $database->dbQuery("SELECT owner_id FROM lots WHERE id = ?", [$lotId])->fetchColumn();
         
-        if ($model->getOne('api_user_tokens', $lot_owner[0]['owner_id'], 'user_id') == null) {
+        if (!$database->dbQuery("SELECT id FROM api_user_tokens WHERE user_id = ?", [$lotOwner])) {
             header('HTTP/1.0 200');
             header('Content-Type: application/json; charset=UTF-8');
             
