6eb5eae82f7d6fb63536bdaba254957e60cf7169
Obolduy <ivanox01@yandex.ru>
2021-10-08 16:57:50 +0300

    config fix

--- config.php
+++ config.php
@@ -10,48 +10,48 @@ define("DEFAULT_DB_CONNECTION", "mysql");
 */
 
 define("SITE_EMAIL", "test-example-mail@kurlyk.su");
-define("EMAIL_HEADERS", ['MIME-Version: ' => 1.0, 'Content-type: ' => 'text/html; charset=iso-8859-1',
-                         'From' => SITE_EMAIL, 'Reply-To' => SITE_EMAIL]);
+define("EMAIL_HEADERS", ["MIME-Version: " => 1.0, "Content-type: " => "text/html; charset=iso-8859-1",
+                         "From" => SITE_EMAIL, "Reply-To" => SITE_EMAIL]);
                          
-define("EMAIL_REGISTRATION_MESSAGE_START", '<html>
+define("EMAIL_REGISTRATION_MESSAGE_START", "<html>
 <head>
   <title>Продолжите Вашу регистрацию</title>
 </head>
 <body>
-  <p>Для завершения регистрации перейдите по этой ссылке: http://likeavito/registration/');
+  <p>Для завершения регистрации перейдите по этой ссылке: http://{$_SERVER['SERVER_NAME']}/registration/");
 
-define("EMAIL_RESET_PASSWORD_MESSAGE_START", '<html>
+define("EMAIL_RESET_PASSWORD_MESSAGE_START", "<html>
 <head>
   <title>Сброс пароля</title>
 </head>
 <body>
-  <p>Для завершения регистрации перейдите по этой ссылке: http://likeavito/user/resetpassword/');
+  <p>Для завершения регистрации перейдите по этой ссылке: http://{$_SERVER['SERVER_NAME']}/user/resetpassword/");
 
-define("EMAIL_ACCOUNT_DELETE_MESSAGE_START", '<html>
+define("EMAIL_ACCOUNT_DELETE_MESSAGE_START", "<html>
 <head>
   <title>Безвозвратное удаление Вашего аккаунта</title>
 </head>
 <body>
-  <p>Для завершения регистрации перейдите по этой ссылке: http://likeavito/user/delete/');
+  <p>Для завершения регистрации перейдите по этой ссылке: http://{$_SERVER['SERVER_NAME']}/user/delete/");
 
-define("EMAIL_CHANGE_EMAIL_MESSAGE_START", '<html>
+define("EMAIL_CHANGE_EMAIL_MESSAGE_START", "<html>
 <head>
   <title>Смена пароля</title>
 </head>
 <body>
-  <p>Здравствуйте! С Вашего акакаунта на сайте http://likeavito/ поступил запрос на изменение Email. Если этот запрос делали не Вы,
-  тогда просто проигнорируйте это письмо. Для подтверждения смены Email перейдите по ссылке: http://likeavito/user/change_email/');
+  <p>Здравствуйте! С Вашего акакаунта на сайте http://{$_SERVER['SERVER_NAME']}/ поступил запрос на изменение Email. Если этот запрос делали не Вы,
+  тогда просто проигнорируйте это письмо. Для подтверждения смены Email перейдите по ссылке: http://{$_SERVER['SERVER_NAME']}/user/change_email/");
 
-define("EMAIL_CHANGE_PASSWORD_MESSAGE_START", '<html>
+define("EMAIL_CHANGE_PASSWORD_MESSAGE_START", "<html>
 <head>
   <title>Смена пароля</title>
 </head>
 <body>
-  <p>Здравствуйте! С Вашего акакаунта на сайте http://likeavito/ поступил запрос на изменение пароля. Если этот запрос делали не Вы,
-  тогда просто проигнорируйте это письмо. Для подтверждения смены пароля перейдите по ссылке: http://likeavito/user/change_password/');
+  <p>Здравствуйте! С Вашего акакаунта на сайте http://{$_SERVER['SERVER_NAME']}/ поступил запрос на изменение пароля. Если этот запрос делали не Вы,
+  тогда просто проигнорируйте это письмо. Для подтверждения смены пароля перейдите по ссылке: http://{$_SERVER['SERVER_NAME']}/user/change_password/");
 
-define("EMAIL_MESSAGE_END", '
+define("EMAIL_MESSAGE_END", "
 </p>
 </body>
 </html>
-');
+");
