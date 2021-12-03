47b0897377fd7747ae088db4d021774768ac460e
Obolduy <ivanox01@yandex.ru>
2021-12-03 17:50:44 +0300

    index.php has prepared for Docker

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 ini_set('display_errors', 'off');
+set_include_path(__DIR__);
 
 use App\Controllers\LoginController;
 use App\Router\Router;
@@ -8,10 +9,12 @@ use Dotenv\Dotenv;
 session_start();
 require_once 'vendor/autoload.php';
 
-$routes = require 'App/routes.php';
+$routes = require_once __DIR__.'/App/routes.php';
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+$_SERVER['SERVER_NAME'] = $_ENV['APP_HOST'];
 
 if ($_COOKIE['remember_token']) {
     LoginController::loginByRememberToken($_COOKIE['remember_token']);
